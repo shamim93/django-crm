85bc6088ec05cfc8224c84756304b3c07705d911
Shamim Hossen <shamim41.cse@gmail.com>
2021-04-06 22:54:44 +0600

    admin site registered

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Customer,Product,Order
 
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name','email','phone',)
+    readonly_fields = ('created',)
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','price','category',)
+    readonly_fields = ('created',)
+    
+admin.site.register(Customer,CustomerAdmin)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Order)
