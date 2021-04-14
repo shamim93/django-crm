f542f69ba78ddfa96d1a55432779dd4103cde3e4
Shamim Hossen <shamim41.cse@gmail.com>
2021-04-14 22:07:58 +0600

    urls modified

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,8 @@ app_name='accounts'
 urlpatterns=[
     path('',views.home,name='home'),
     path('product',views.product,name='product'),
+    path('customer/create',views.createcustomer,name='createcustomer'),
+    path('order/create',views.createOrder,name='createorder'),
+    path('order/edit/<int:order_id>/',views.editOrder,name='editorder'),
     path('customer/<int:customer_id>',views.customer,name='customer'),
 ]
